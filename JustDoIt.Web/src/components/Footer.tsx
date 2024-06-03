902395f6fd60e0f426fa6393cc6617bc7324fe96
TheOfficialDarac <dario.rupcic2003@gmail.com>
2024-06-03 12:04:09 +0200

    test from place

--- JustDoIt.Web/src/components/Footer.tsx
+++ JustDoIt.Web/src/components/Footer.tsx
@@ -1,6 +1,6 @@
-type Props = {
+interface Props {
   name: string;
-};
+}
 
 const Footer = ({ name }: Props) => {
   return <div className="container p-4 bg-green-300">Footer {name}</div>;
--- JustDoIt.Web/src/components/Header.tsx
+++ JustDoIt.Web/src/components/Header.tsx
@@ -1,9 +1,9 @@
-type Props = {
+interface Props {
   name: string;
-};
+}
 
 const Header = ({ name }: Props) => {
-  return <div className="container bg-red-300 p-4">Header {name}</div>;
+  return <div className="container-xl bg-red-300 p-4">Header {name}</div>;
 };
 
 export default Header;
